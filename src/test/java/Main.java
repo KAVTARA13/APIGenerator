5f6a4e5222ad36f0d706fdb688bdb84ad100bcfe
unknown <lukakavtaradze2001@gmail.com>
2022-12-19 03:14:12 +0400

    opaaaa

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,35 +1,70 @@
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 
 public class Main {
+    protected void start() {
+        ServerSocket s;
+        String gets = "";
 
-    public static void main( String[] args ) throws IOException {
-        final int port = 80;
-        final ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Start on port 8888");
+        try {
+            // create the main server socket
+            s = new ServerSocket(8888);
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+            return;
+        }
 
-        System.out.println(String.format("Start listening on port %d", port));
-        while (true) {
-            final Socket client = serverSocket.accept();
-            final InputStream inputStream = client.getInputStream();
-            final OutputStream outputStream = client.getOutputStream();
+        System.out.println("Waiting for connection");
+        for (;;) {
+            try {
+                Socket socket = s.accept();
+                //socket is an instance of Socket
+                InputStream is = socket.getInputStream();
+                InputStreamReader isReader = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isReader);
+                System.out.println("+++++++++++++++++++++++++++++");
+                System.out.println("SERVER SOCKET TESTS:");
+                System.out.println("getChannel: " + s.getChannel());
+                System.out.println("getInetAddress: " + s.getInetAddress());
+                System.out.println("getLocalPort: " + s.getLocalPort());
+                System.out.println("getLocalSocketAddress: " + s.getLocalSocketAddress());
 
-            new Thread(() -> {
-                try{
-                    final ByteArrayOutputStream request = new ByteArrayOutputStream();
-                    transfer(inputStream,request);
-                    System.out.println(request.toString());
-                }catch (IOException ex){
-                    ex.printStackTrace();
+                System.out.println();
+
+                System.out.println("CLIENT SOCKET TESTS:");
+                System.out.println("getChannel: " + socket.getChannel());
+                System.out.println("getLocalAddress: " + socket.getLocalAddress());
+                System.out.println("getLocalPort: " + socket.getLocalPort());
+                System.out.println("getLocalSocketAddress: " + socket.getLocalSocketAddress());
+                System.out.println("getRemoteSocketAddress: " + socket.getRemoteSocketAddress());
+                System.out.println("getInetAddress: " + socket.getInetAddress());
+                System.out.println("getInputStream: " + socket.getInputStream());
+                System.out.println("getOutputStream: " + socket.getOutputStream());
+
+                System.out.println("+++++++++++++++++++++++++++++");
+//code to read and print headers
+                String headerLine = null;
+                while((headerLine = br.readLine()).length() != 0){
+                    System.out.println(headerLine);
                 }
-            }).start();
+
+//code to read the post payload data
+                StringBuilder payload = new StringBuilder();
+                while(br.ready()){
+                    payload.append((char) br.read());
+                }
+                System.out.println("Payload data is: "+payload.toString());
+
+
+            } catch (Exception e) {
+                System.out.println("Error: " + e);
+            }
         }
     }
-    public static void transfer(InputStream inputStream,OutputStream outputStream)throws IOException {
-        final byte[] buff = new byte[2048];
-        int read;
-        while ((read = inputStream.read(buff, 0, Math.min(inputStream.available(), 2048))) > 0) {
-            outputStream.write(buff, 0, read);
-        }
+
+    public static void main(String args[]) {
+        Main ws = new Main();
+        ws.start();
     }
 }
