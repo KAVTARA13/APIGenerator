e177a935d5d937601a7412e3d731447e7d79a751
unknown <lukakavtaradze2001@gmail.com>
2022-12-25 03:11:28 +0400

    GET problem

--- src/test/java/BrowserMob.java
+++ src/test/java/BrowserMob.java
@@ -7,75 +7,55 @@ import io.netty.handler.codec.http.HttpResponse;
 import net.lightbody.bmp.BrowserMobProxyServer;
 import net.lightbody.bmp.core.har.Har;
 import net.lightbody.bmp.filters.RequestFilter;
+import net.lightbody.bmp.filters.ResponseFilter;
 import net.lightbody.bmp.proxy.CaptureType;
+import net.lightbody.bmp.util.HttpMessageContents;
+import net.lightbody.bmp.util.HttpMessageInfo;
 import org.littleshoot.proxy.HttpFilters;
 import org.littleshoot.proxy.HttpFiltersSource;
 
 import java.io.*;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
+import java.util.*;
 
 
 public class BrowserMob {
     public static BrowserMobProxyServer proxy;
     public static void main(String[] args) {
+        final String[] URL = {""};
         proxy = new BrowserMobProxyServer();
         proxy.setTrustAllServers(true);
         proxy.setHarCaptureTypes(CaptureType.getAllContentCaptureTypes());
         proxy.enableHarCaptureTypes(CaptureType.getAllContentCaptureTypes());
-        proxy.newHar();
-//        proxy.addHttpFilterFactory(new org.littleshoot.proxy.HttpFiltersSource(){
-//            @Override
-//            public HttpFilters filterRequest(HttpRequest httpRequest, ChannelHandlerContext channelHandlerContext) {
-//                System.out.println("gaeshva");
-//                return null;
-//            }
-//
-//            @Override
-//            public int getMaximumRequestBufferSizeInBytes() {
-//                int maxBufferSize = 0;
-//                for (HttpFiltersSource source : filterFactories) {
-//                    int requestBufferSize = source.getMaximumRequestBufferSizeInBytes();
-//                    if (requestBufferSize > maxBufferSize) {
-//                        maxBufferSize = requestBufferSize;
-//                    }
-//                }
-//                return maxBufferSize;
-//            }
-//
-//            @Override
-//            public int getMaximumResponseBufferSizeInBytes() {
-//                return 0;
-//            }
-//        } );
         proxy.addRequestFilter(new RequestFilter() {
             @Override
             public HttpResponse filterRequest(HttpRequest httpRequest, net.lightbody.bmp.util.HttpMessageContents httpMessageContents, net.lightbody.bmp.util.HttpMessageInfo httpMessageInfo) {
-                SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy_HH.mm.ss");
-                Date date = new Date();
-//                System.out.println(formatter.format(date));
-                System.out.println("URI: "+httpRequest.getUri());
-                System.out.println("result: "+httpRequest.getDecoderResult().toString());
-                System.out.println("method: "+httpRequest.getMethod().toString());
-                System.out.println("--------------------------------");
-                System.out.println(httpMessageContents.getTextContents());
-                System.out.println(Arrays.toString(httpMessageContents.getBinaryContents()));
-
-//                Har har = proxy.getHar();
-//                File harFile = new File(formatter.format(date)+".har");
-//                try {
-//                    har.writeTo(harFile);
-//                } catch (IOException e) {
-//                    throw new RuntimeException(e);
-//                }
+                if (Arrays.toString(httpRequest.headers().entries().toArray()).contains("User-Agent=Postman")){
+                    if (httpRequest.getMethod().toString().equals("CONNECT")){
+                        URL[0] = httpRequest.getUri().split(":")[0];
+                    }else if(!URL[0].equals("")){
+                        System.out.println("URI: "+URL[0]+httpRequest.getUri());
+                        System.out.println("headers: "+Arrays.toString(httpRequest.headers().entries().toArray()));
+                        System.out.println("result: "+httpRequest.getDecoderResult().toString());
+                        System.out.println("method: "+httpRequest.getMethod().toString());
+                        System.out.println(httpMessageContents.getTextContents());
+                    }
+                }
                 return null;
             }
         });
+        proxy.addResponseFilter(new ResponseFilter() {
+            @Override
+            public void filterResponse(HttpResponse httpResponse, HttpMessageContents httpMessageContents, HttpMessageInfo httpMessageInfo) {
+                if (Arrays.toString(httpResponse.headers().entries().toArray()).contains(URL[0]) && !URL[0].equals("")){
+                    System.out.println();
+                    System.out.println("headers: "+Arrays.toString(httpResponse.headers().entries().toArray()));
+                    System.out.println("getStatus: "+httpResponse.getStatus());
+                    System.out.println("result: "+httpResponse.getDecoderResult().toString());
+                    System.out.println(httpMessageContents.getTextContents());
+                }
+            }
+        });
         proxy.start(44);
-//        HarReader harReader = new HarReader();
-//        de.sstoehr.harreader.model.Har har = harReader.readFromFile(new File("aafga.har"));
-//        System.out.println(har.getLog().getEntries().get(0).getResponse().getContent().getText());
     }
 }
-
